63d823662ecb881c2987ee7041c9d895559a3f1e
JaeSeungJae <valkyrie9923@gmail.com>
2024-12-15 04:05:13 +0900

    feat: getProduct 연결 - db 구조 문제로 인해 seller_id 불러오기 실패

--- Frontend/shopping_mall/src/pages/Market/MarketDetail.jsx
+++ Frontend/shopping_mall/src/pages/Market/MarketDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { PageContainer, Header, ProductInfo, ImagesContainer, ImageBox, ButtonContainer, ActionButton,
     ProductDetailInfo
@@ -6,13 +6,37 @@ import { PageContainer, Header, ProductInfo, ImagesContainer, ImageBox, ButtonCo
 
 const MarketDetail = () => {
     const {id} = useParams();
+    const [productInfo, setProductInfo] = useState();
     const navigate = useNavigate();
-    
+    useEffect(()=> {
+      getProductInfo();
+    }, [])
+    const getProductInfo = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/rest/getProduct`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            uid: id
+          })
+        });
+        if (response) {
+          const data = await response.json();
+          console.log(data);
+          setProductInfo(data.data);
+        }
+      }
+      catch {
+
+      }
+    }
     return (
         <PageContainer>
           {/* 상단 영역 */}
           <Header>
-            <h2>(product_name)</h2>
+            <h2>{productInfo.name}</h2>
             <div>
               <button style={{margin: '5px'}}>관심</button>
               <button style={{margin: '5px'}}>수정</button>
@@ -25,7 +49,7 @@ const MarketDetail = () => {
           <ProductDetailInfo>
             <p>판매자 : (seller_id)</p>
             <p>상태 : (status)</p>
-            <p>가격 : (product_price)</p>
+            <p>가격 : {`${productInfo.price.toLocaleString('ko-Kr')}원`}</p>
           </ProductDetailInfo>
     
           {/* 상품 이미지 */}
@@ -37,7 +61,7 @@ const MarketDetail = () => {
     
           {/* 상품 설명 */}
           <ProductInfo>
-            <p>상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. 상품{id}의 설명입니다. </p>
+            <p>{productInfo.description}</p>
           </ProductInfo>
     
           {/* 버튼 */}
